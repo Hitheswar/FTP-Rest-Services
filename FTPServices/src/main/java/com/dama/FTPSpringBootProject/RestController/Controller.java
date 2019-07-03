8a750729ba2fd5af160261f6eb7f3f2c0b9ee297
hitheswar <dama.hitheswar@aline-consulting.com>
2019-07-03 20:40:31 +0530

    sdds

--- FTPServices/src/main/java/com/dama/FTPSpringBootProject/RestController/Controller.java
+++ FTPServices/src/main/java/com/dama/FTPSpringBootProject/RestController/Controller.java
@@ -72,7 +72,8 @@ public class Controller {
 		
         InputStream inputStream = FTPClient.retrieveFileStream(path);
         System.out.println("stream :");
-        Files.copy(inputStream, file.toPath());
+        OutputStream outputStream = new FileOutputStream(file);
+        Files.copy(inputStream, new File("/root/temp/"+fileName).toPath());
         System.out.println("copy stream :");
 
         Path path1 = Paths.get(file.getAbsolutePath());
